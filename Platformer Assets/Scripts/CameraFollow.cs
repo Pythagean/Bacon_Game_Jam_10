e6c50febe7bed6f09fd7e335f5e4470606ec6dfc
ralius18 <brayds97@gmail.com>
2016-01-22 21:32:25 +1300

    Camera
    CameraFollow = 1D (Locked Y axis)
    SmoothCamera2D = 2D

---
+++ Platformer Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing = 5f;
+
+	Vector3 offset; 
+
+	void Start()
+	{
+		offset = transform.position - target.position;
+	}
+
+	void FixedUpdate ()
+	{
+		Vector3 targetCamPos = new Vector3(target.position.x + offset.x, transform.position.y, target.position.z + offset.z);
+		transform.position = Vector3.Lerp (transform.position, new Vector3 (targetCamPos.x , 0.0f , targetCamPos.z) , smoothing * Time.deltaTime);
+	}
+}
