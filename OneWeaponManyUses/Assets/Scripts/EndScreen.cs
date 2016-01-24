cd30f7f05ab166ff65b7120e655b1f432ee36a10
ralius18 <brayds97@gmail.com>
2016-01-24 21:37:00 +1300

    End and a lot of metadata

---
+++ OneWeaponManyUses/Assets/Scripts/EndScreen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndScreen : MonoBehaviour {
+
+	//public string lastLevel;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.anyKeyDown)
+		{
+			Application.LoadLevel ("MainMenu");
+		}
+
+	}
+}
